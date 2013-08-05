29f6d5f57d1a29cc36979e7eb04ea7a8be92454e
Dustin Zeisler <dustin@zive.me>
2013-08-05 11:48:38 -0700

    sql in model for finding nearest points

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,3 +1,14 @@
 class Dealer < ActiveRecord::Base
   belongs_to :licence
+
+  def within(radius, lat, lng)
+   ActiveRecord::Base.connection.execute(
+    "SELECT lat, lng,
+      ( 3959 * acos( cos( radians(45.53299) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122.6894) ) + sin( radians(45.53299) ) * sin( radians( lat ) ) ) )
+    AS distance
+    from dealers
+    where ( 3959 * acos( cos( radians(45.53299) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122.6894) ) + sin( radians(45.53299) ) * sin( radians( lat ) ) ) ) < #{ActiveRecord::Base.sanitize radius}
+    ORDER BY distance limit 10;")
+  end
+
 end
