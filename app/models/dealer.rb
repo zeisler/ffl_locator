c11a22f64de7ca06d2c0cbbcc8bb9c218254d409
Dustin Zeisler <dustin@zive.me>
2013-08-05 12:14:40 -0700

    dealers model can be asked for all points around a point by radius

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,14 +1,19 @@
 class Dealer < ActiveRecord::Base
   belongs_to :licence
 
-  def within(radius, lat, lng)
-   ActiveRecord::Base.connection.execute(
-    "SELECT lat, lng,
-      ( 3959 * acos( cos( radians(45.53299) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122.6894) ) + sin( radians(45.53299) ) * sin( radians( lat ) ) ) )
-    AS distance
-    from dealers
-    where ( 3959 * acos( cos( radians(45.53299) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122.6894) ) + sin( radians(45.53299) ) * sin( radians( lat ) ) ) ) < #{ActiveRecord::Base.sanitize radius}
-    ORDER BY distance limit 10;")
+  def self.within(radius, lat, lng)
+    results_raw = ActiveRecord::Base.connection.execute(
+      "SELECT id,
+        ( 3959 * acos( cos( radians(45.53299) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122.6894) ) + sin( radians(45.53299) ) * sin( radians( lat ) ) ) )
+      AS distance
+      from dealers
+      where ( 3959 * acos( cos( radians(45.53299) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122.6894) ) + sin( radians(45.53299) ) * sin( radians( lat ) ) ) ) < #{ActiveRecord::Base.sanitize radius}
+      ORDER BY distance limit 10;")
+    results_raw.map do |row|
+       {Dealer: Dealer.find_by_id(row["id"]), distance: row["distance"]}
+    end
+
+
   end
 
 end
